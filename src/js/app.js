39a7633eaa24112d6c8c2f1190161f8cfe8baceb
Carolina Chagas <thamelo.win@gmail.com>
2023-12-07 12:54:00 +0100

    updated grayblack cars

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,29 @@
-const redColor = document.getElementsByClassName("red");
-const imgCard = document.querySelector ("product-card");
-const cardbutton = document.getElementById ("button");
-const itemtag = document.getElementsByName ("h3");
+const redColor = document.getElementsByClassName('red');
+const imgCard = document.querySelector ('product-card');
+const cardbutton = document.getElementById ('button');
+const tagname = document.querySelector ('h3');
 
-const blackColor = document.getElementsByClassName("black");
 
-const grayColor = document.getElementsByClassName ("gray");
-
-redColor.addeventlistener ("click"),function (){
+redColor.addeventlistener ('click'),function (){
 imgCard.computedStyleMap.backgroundimage= 'URL ("../../public/img/redcar.jpg")';
-cardbutton.stylebackgroundcolor = "red"
-itemtag.style.backgroundcolor = "red" };
+cardbutton.stylebackgroundcolor = 'red'
+tagname.style.backgroundcolor = 'red';
+};
+
+const blackColor = document.getElementsByClassName('black');
+const imgCardblack = document.querySelector ('product-card');
+const cardbuttonblack = document.getElementById ('button');
+const tagnamblack = document.querySelector ('h3');
+
+blackColor.addeventlistener ('click'),function (){
+    imgCard.computedStyleMap.backgroundimage= 'URL ("../../public/img/blackcar.jpg")';
+    cardbutton.stylebackgroundcolor = 'black'
+    tagname.style.backgroundcolor = 'black';
+    };
+
+const grayColor = document.getElementsByClassName ('gray');
+grayColor.addeventlistener ('click'),function (){
+    imgCard.computedStyleMap.backgroundimage= 'URL ("../../public/img/graycar.jpg")';
+    cardbutton.stylebackgroundcolor ='gray'
+    tagname.style.backgroundcolor ='gray';
+    };
